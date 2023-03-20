cbdd12ec549bf6498a2df776e6da182a5a406729
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-03-20 23:48:00 +0600

    Just created

---
+++ SwiftUI Resources/SwiftUI_ResourcesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ResourcesApp.swift
+//  SwiftUI Resources
+//
+//  Created by Md Abir Hossain on 16/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ResourcesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
